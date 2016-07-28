08caad981bfc017219f3f564cc67f9c5e64f6fd7
Bennett Batzli <bennett.batzli@gmail.com>
2016-07-28 18:19:24 -0500

    users may create comments now

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+# Users may create comments now
 ActiveRecord::Schema.define(version: 20160726234218) do
 
   create_table "articles", force: :cascade do |t|
